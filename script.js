442555b4eb846223fbdb04560ad705cf90a1c81b
keller-johnson <keller.s.johnson@gmail.com>
2022-11-22 17:18:56 -0800

    I finally got it to work

--- script.js
+++ script.js
@@ -8,37 +8,20 @@ const count = 10;
 const apiKey = "guudGZn5Tz4ppMxQqwyiqrEZlZGBHJY0nrpo_qTbniM";
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
-//helper function to set attribues on DOM elements
-function setAttributes(element, attributes) {
-  for (const key in attributes) {
-    element.setAttributes(key, attributes[key]);
-  }
-}
-
-//create elements for Link & photos, add to dom
+//Create Elements For Links & Photos, Add to DOM
 function displayPhotos() {
-  //Run function for each object in photosArray
+  //run function for each object in photosArray
   photosArray.forEach((photo) => {
-    // create <a> to link to Unsplash
+    //create <a> to link to unsplash
     const item = document.createElement("a");
-    // item.setAttribute("href", photo.links.html);
-    // item.setAttribute("target", "_blank");
-    setAttributes(item, {
-      href: photo.links.html,
-      target: "_blank",
-    });
-    //create <img> for photo
+    item.setAttribute("href", photo.links.html);
+    item.setAttribute("target", "_blank");
+    //Create <img> for photo
     const img = document.createElement("img");
-    // img.setAttribute("src", photo.url.regular);
-    // img.setAttribute("alt", photo.alt_description);
-    // img.setAttribute("title", photo.alt_description);
-    setAttributes(img, {
-      src: photo.urls.regular,
-      alt: photo.alt_description,
-      title: photo.alt_description,
-    });
-
-    // put <img> inside <a> then put both inside imageContainer Element
+    img.setAttribute("src", photo.urls.regular);
+    img.setAttribute("alt", photo.alt_description);
+    img.setAttribute("title", photo.alt_description);
+    //put the <img> inside <a>, then put both inside imageContainer Element
     item.appendChild(img);
     imageContainer.appendChild(item);
   });
@@ -49,7 +32,6 @@ async function getPhotos() {
   try {
     const response = await fetch(apiUrl);
     photosArray = await response.json();
-    console.log(photosArray);
     displayPhotos();
   } catch (error) {
     //catch error here
